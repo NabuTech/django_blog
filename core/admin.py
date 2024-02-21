b37fd6250cc3deaf14568499a9af72a2f784923e
sj <nabutech@proton.me>
2024-02-21 23:26:33 +0000

    Broken user changes

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import UserProfile
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, UserProfile
 
+admin.site.register(CustomUser, UserAdmin)
 admin.site.register(UserProfile)
--- core/models.py
+++ core/models.py
@@ -1,12 +1,18 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class CustomUser(AbstractUser):
     about = models.TextField(blank=True)
     twitter_link = models.URLField(blank=True)
     linkedin_link = models.URLField(blank=True)
     github_link = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.username
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+   
 
     def __str__(self):
         return self.user.username
